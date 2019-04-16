0dd40b3d225b9bac3a4ce34fc3819dbe757e953f
Ammon Corl <Ammon@vlsb-3188-2.mvz.berkeley.edu>
2019-04-16 08:53:40 -0700

    1st website upload

---
+++ Ammon_Corl_Homepage/Genetics_of_polymorphism_files/Genetics_of_polymorphism.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20190416
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Genetics_of_polymorphism_files/Genetics_of_polymorphismMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Ammon_Corl_Homepage/Polymorphism_maintenance_files/Polymorphism_maintenance.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20190416
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Polymorphism_maintenance_files/Polymorphism_maintenanceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
