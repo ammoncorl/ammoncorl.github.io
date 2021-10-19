52deedc4601f093ccf518ab252646109fb43de75
ammoncorl <49661692+ammoncorl@users.noreply.github.com>
2021-10-19 16:35:52 -0700

    Add files via upload
    Update to website 10-10-21

--- Ammon_Corl_Homepage/Publications_files/Publications.js
+++ Ammon_Corl_Homepage/Publications_files/Publications.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20190416
+// Created by iWeb 3.0.4 local-build-20211019
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Publications_files/PublicationsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
